cb79d1517535c8b2484026367ef3d3a04d0eadee
Marwa Belal <marwabelal105@gmail.com>
2019-03-21 19:05:55 +0200

    Add files via upload

--- Compilers#1/src/compilers/pkg1/Compilers1.java
+++ Compilers#1/src/compilers/pkg1/Compilers1.java
@@ -9,6 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -19,53 +20,36 @@ import java.lang.Object;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-/**
- *
- * @author Nagham
- */
 public class Compilers1 {
 
-    private static final String FILENAME = "C:\\Users\\Mariam\\Documents\\GitHub\\mini-C-compiler\\REs.txt";
+    public static String readFileAsString(String fileName) throws Exception {
+        String data = "";
+        data = new String(Files.readAllBytes(Paths.get(fileName)));
+        return data;
+    }
+    private static final String FILENAME = "C:\\Users\\Belal\\Desktop\\REs.txt";
 
-    public static String readFileAsString(String fileName)throws Exception 
-  { 
-    String data = ""; 
-    data = new String(Files.readAllBytes(Paths.get(fileName))); 
-    return data; 
-  } 
-    
     public static void main(String[] args) throws Exception {
-        String input = readFileAsString("C:\\Users\\Mariam\\Documents\\GitHub\\mini-C-compiler\\test.txt");
+        String data = readFileAsString("C:\\Users\\Belal\\Desktop\\test.txt");
         ArrayList<Pair<String, String>> output = new ArrayList<Pair<String, String>>();
         BufferedReader br = null;
         FileReader fr = null;
         List<String> sInput = new ArrayList<String>();
-        /*String input = "bool isPowerOfTwo (int x)\n"
-                + "{ \n"
-                + "// First x in the below expression is\n"
-                + "// for the case when x is 0\n"
-                + "return x || ( !(x & (x - 1))) x;\n"
-                + "<<}";*/
         String x[];
-        System.out.println(input);
-        String lines[] = input.split("\n|(?<=\\n)");
+        System.out.println(data);
+        String lines[] = data.split("\n|(?<=\\n)");
         int counter = 0;
         for (int i = 0; i < lines.length; i++) {
-            System.out.println("mariam");
             System.out.println(i + lines[i]);
             if (lines[i].contains("//")) {
-                System.out.println("nagham");
                 sInput.add(lines[i]);
                 continue;
             }
             x = lines[i].split("(?<=\\(|\\[|\\{|=|\\+|-|/|\\*|!)|(?=\\)|\\]|\\}|=|\\+|-|/|\\*|!)|(?=\\;)|(?=\\,)|( |$)(?=(([^\"]*\"){2})*[^\"]*$)|(?=(([^\']*\'){2})*[^\']*$)( |$) ");
             for (int j = 0; j < x.length; j++) {
-                System.out.println("feeeen");
                 System.out.println(x[j]);
                 sInput.add(x[j]);
             }
-          
-
         }
         for (int i = 0; i < sInput.size(); i++) {
             sInput.set(i, sInput.get(i).trim());
@@ -79,49 +63,40 @@ public class Compilers1 {
                 String sCurrentLine;
                 String record;
                 String regex[];
+                System.out.println(sInput.get(i));
                 while ((record = br.readLine()) != null) {
                     regex = record.split(" ");
                     Pattern pattern = Pattern.compile(regex[0]);
                     Matcher matcher = pattern.matcher(sInput.get(i));
-
                     if (matcher.find()) {
                         output.add(new Pair<String, String>(sInput.get(i), regex[1]));
                         break;
                     }
                 }
-
             } catch (IOException e) {
-
                 e.printStackTrace();
-
             } finally {
-
                 try {
-
                     if (br != null) {
                         br.close();
                     }
-
                     if (fr != null) {
                         fr.close();
                     }
-
                 } catch (IOException ex) {
-
                     ex.printStackTrace();
-
                 }
             }
-
         }
-
+        PrintWriter out = new PrintWriter("C:\\Users\\Belal\\Desktop\\output.txt");
         for (int i = 0; i < output.size(); i++) {
             System.out.print(output.get(i).getKey());
             System.out.print(" : ");
             System.out.println(output.get(i).getValue());
+            out.println(output.get(i).getValue() + " : " + output.get(i).getKey());
         }
         System.out.println(output.size());
         System.out.println(sInput.size());
-
+        out.close();
     }
 }
