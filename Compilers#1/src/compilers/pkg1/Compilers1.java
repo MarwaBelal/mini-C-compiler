429f5fdcdfa04e038335d4a2d23f5c3cc968af31
Maryam Ehab <maryammehab@gmail.com>
2019-03-22 14:03:19 +0200

    phase1

--- Compilers#1/src/compilers/pkg1/Compilers1.java
+++ Compilers#1/src/compilers/pkg1/Compilers1.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package compilers.pkg1;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -20,10 +16,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
-/**
- *
- * @author Nagham
- */
+
 public class Compilers1 {
 
     public static String readFileAsString(String fileName) throws Exception {
@@ -32,34 +25,24 @@ public class Compilers1 {
         return data;
     }
 
-    private static final String FILENAME = "C:\\Users\\LENOVO\\Desktop\\REs.txt";
-    //private static final String FILENAME = "C:\\Users\\LENOVO\\Documents\\GitHub\\mini-C-compiler\\REs.txt";
+    private static final String FILENAME = "C:\\Users\\Mariam\\Documents\\GitHub\\mini-C-compiler\\Compilers#1\\REs.txt";
 
     public static void main(String[] args) throws Exception {
-        String input = readFileAsString("C:\\Users\\LENOVO\\Desktop\\test.txt");
-        ArrayList<Pair<String, String>> output
-                = new ArrayList<Pair<String, String>>();
+        String input = readFileAsString("C:\\Users\\Mariam\\Documents\\GitHub\\mini-C-compiler\\Compilers#1\\test.txt");
+        ArrayList<Pair<String, String>> output = new ArrayList<Pair<String, String>>();
         BufferedReader br = null;
         FileReader fr = null;
-        //String input = "(if) ( int) static 0 90 111 1nagham nagham3 _1 auto yes-yes 0.0 12 s, m; \"bla bla\" 'z' x1=x2 /x3 int intvalue = 10.0+5;  ";
-        //String sInput[] = new String[100];
         List<String> sInput = new ArrayList<String>();
-        /*String input = "bool isPowerOfTwo (int x)\n" +
-         "{ \n" +
-         "// First x in the below expression is\n" +
-         "// for the case when x is 0\n" +
-         "return x || ( !(x & (x - 1))) x;\n" +
-         "<<}";*/
         String x[];
         System.out.println(input);
-        //sInput = input.split(" (?<=//)");
-        //sInput = input.split("(?=\\))|(?<=\\()|(?<=\")|(?=\")|( |$)(?=(([^\"]*\"){2})*[^\"]*$)");
         String lines[] = input.split("\n|(?<=\\n)");
         int counter = 0;
         String multi = "";
         String temp = "";
         for (int i = 0; i < lines.length; i++) {
-            //System.out.println(i + lines[i]);
+            if (lines[i].length() > 2 && lines[i].substring(lines[i].length() - 3, lines[i].length()).contains("\\n")) {
+                lines[i] = lines[i].substring(0, lines[i].length() - 3);
+            }
             if (lines[i].contains("//")) {
                 sInput.add(lines[i]);
                 continue;
@@ -71,104 +54,87 @@ public class Compilers1 {
             if (lines[i].contains("/*")) {
                 multi += lines[i].trim();
                 System.out.println("___________________________________________________");
-                //System.out.println(multi);  
                 for (int j = i + 1; j < lines.length; j++) {
                     counter++;
-                    if(lines[j].equals("")){continue;}
-                    //System.out.print("j = "+ j);
-                    System.out.println("line: " + lines[j]);
+                    if (lines[j].equals("")) {
+                        continue;
+                    }
+                    //System.out.println("line: " + lines[j]);
                     if (lines[j].contains("*/")) {
-                        multi += " "+lines[j].trim();
-                        System.out.println("end");
+                        multi += " " + lines[j].trim();
+                        //System.out.println("end");
 
                     } else {
-                        System.out.println("not end");
-                        multi += " "+lines[j].trim();
+                        //System.out.println("not end");
+                        multi += " " + lines[j].trim();
                     }
                 }
-                System.out.println("*********************************");
-                System.out.println(multi);
                 sInput.add(multi);
                 i += counter;
                 continue;
             }
             x = lines[i].split("(?<=\\(|\\[|\\{|=|\\+|-|/|\\*|!)|(?=\\)|\\]|\\}|=|\\+|-|/|\\*|!)|(?=\\;)|(?=\\,)|( |$)(?=(([^\"]*\"){2})*[^\"]*$)|(?=(([^\']*\'){2})*[^\']*$)( |$) ");
             for (int j = 0; j < x.length; j++) {
-                //System.out.println(x[j]);
                 sInput.add(x[j]);
             }
-
         }
-        //sInput = input.split("(?<=\\(|\\+|\\*|-|=|/)|(?=\\)|\\*|\\+|-|=|/)|(?=\\;)|(?=\\,)|( |$)(?=(([^\"]*\"){2})*[^\"]*$)|(?=(([^\']*\'){2})*[^\']*$)( |$) ");
-
         for (int i = 0; i < sInput.size(); i++) {
             sInput.set(i, sInput.get(i).trim());
-            System.out.println(sInput.get(i));
+            //System.out.println(sInput.get(i));
         }
-
-        for (int i = 0; i < sInput.size(); i++) {
+        List<String> sInput2 = new ArrayList<String>();
+        for (int y = 0; y < sInput.size(); y++) {
+            if (sInput.get(y).isEmpty()) {
+                continue;
+            }
+            sInput2.add(sInput.get(y));
+        }
+        for (int i = 0; i < sInput2.size(); i++) {
             try {
-                //br = new BufferedReader(new FileReader(FILENAME));
                 fr = new FileReader(FILENAME);
                 br = new BufferedReader(fr);
-
-                //for (int k = 0; k < sInput.length; k++) {
-                //    System.out.println(sInput[k]);
-                //}
                 String sCurrentLine;
                 String record;
                 String regex[];
-                Boolean flag= false;
-                //System.out.println(sInput.get(i));
+                Boolean flag = false;
                 while ((record = br.readLine()) != null) {
-                    //System.out.println(record);
                     regex = record.split(" ");
-                    //System.out.println(regex[0]);
                     Pattern pattern = Pattern.compile(regex[0]);
-                    Matcher matcher = pattern.matcher(sInput.get(i));
-
+                    Matcher matcher = pattern.matcher(sInput2.get(i));
                     if (matcher.find()) {
                         flag = true;
-                        output.add(new Pair<String, String>(sInput.get(i), regex[1]));
+                        output.add(new Pair<String, String>(sInput2.get(i), regex[1]));
                         break;
                     }
                 }
-                if(flag==false){
-                    output.add(new Pair<String, String>(sInput.get(i),"<error:didn't match>"));
+                if (flag == false) {
+                    output.add(new Pair<String, String>(sInput2.get(i), "<error:didn't match>"));
                 }
-
             } catch (IOException e) {
-
                 e.printStackTrace();
-
             } finally {
-
                 try {
-
                     if (br != null) {
                         br.close();
                     }
-
                     if (fr != null) {
                         fr.close();
                     }
-
                 } catch (IOException ex) {
-
                     ex.printStackTrace();
-
                 }
             }
-
         }
-
+        PrintWriter out = new PrintWriter("C:\\Users\\Mariam\\Documents\\GitHub\\mini-C-compiler\\Compilers#1\\output.txt");
         for (int i = 0; i < output.size(); i++) {
             System.out.print(output.get(i).getKey());
             System.out.print(" : ");
             System.out.println(output.get(i).getValue());
+            out.println(output.get(i).getValue() + " : " + output.get(i).getKey());
         }
-        System.out.println(output.size());
-        System.out.println(sInput.size());
+        //System.out.println(output.size());
+        //System.out.println(sInput2.size());
+        out.close();
 
     }
 }
